24326210c782a3ef8f7f3d77e4389692538b965d
Meishin <support@onmyodev.com>
2026-02-14 17:38:09 +0800

    Custom search form.

---
+++ searchform.php
@@ -0,0 +1,9 @@
+<form role="search" method="get" class="search-form minimalistflex-search-form" action="<?php echo esc_url( home_url( '/' ) ) ?>">
+    <label>
+        <span class="screen-reader-text">搜索：</span>
+        <input type="search" class="search-field minimalistflex-search-field" placeholder="搜索…" value="" name="s">
+    </label>
+    <button type="submit" class="search-submit minimalistflex-search-submit" aria-label="搜索">
+        <span class="dashicons dashicons-search"></span>
+    </button>
+</form>
