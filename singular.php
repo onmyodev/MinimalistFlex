7e4b9f6ea25a7d95ca8477012bedba05c35e307a
Kyoukawa Meishin <hufang2007@icloud.com>
2026-02-16 22:46:08 +0800

    Fancier debug message because why not ;)

--- singular.php
+++ singular.php
@@ -10,7 +10,7 @@ if ( !defined( 'WPINC' ) ) {
         the_post();
         $mf_id = get_the_author_meta( 'ID' );
     ?>
-    <div class="debug warning"><?php echo get_post_format(); ?></div>
+    <div class="debug warning"><p>Post Format: <?php echo get_post_format(); ?></p></div>
     <?php $supported_formats = get_theme_support( 'post-formats' ); ?>
     <?php $format = get_post_format() ? get_post_format() : 'standard'; ?>
     <?php if ( in_array( $format, $supported_formats[0] ) ): ?>
